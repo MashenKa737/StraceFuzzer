f0f8bd84da99e4b8aad79dcc357a11afdf26babd
ZharkovIlia <zharkov1243@mail.ru>
2017-10-10 18:28:48 +0300

    added class for output management

--- fuzzer.py
+++ fuzzer.py
@@ -57,6 +57,9 @@ class Fault:
     def when(self):
         return self._when
 
+    def all_parts(self):
+        return dict(syscall=self.syscall, error=self.error, when=self.when)
+
     def __str__(self):
         return "fault=" + self._syscall + ":error=" + self._error + ":when=" + str(self._when)
 
@@ -334,6 +337,43 @@ class ExecutionController:
 
         reporter.unwatch()
         exit(0)
+
+
+class ListSuccessfulInjections:
+    def __init__(self, output=sys.stderr):
+        self._header = '--- list of injections, which induced SIGSEGV in targeted executable ---'
+        self._tail = ' end of list '.center(len(self._header), '-')
+        self._separator = "".ljust(len(self._header), '-')
+
+        self._entries = []
+        self._header_printed = False
+
+        if isinstance(output, (str, bytes, int)):
+            self._output = open(output, mode="wt")
+        else:
+            self._output = output
+
+    def add_entry(self, fault: Fault, context: str):
+        self._entries.append(dict(fault=fault, context=context))
+        self.print()
+
+    def print(self):
+        if not self._header_printed:
+            print(self._header, file=self._output)
+            self._header_printed = True
+        for entry in self._entries:
+            print(self._separator, file=self._output)
+            print("Syscall: {syscall}\nError: {error}\nAppearance: {when}".
+                  format(**entry["fault"].all_parts()), file=self._output)
+
+            # TODO add more clever printing context if it is too large
+            print("Context: {}".format(entry["context"]), file=self._output)
+
+        self._entries.clear()
+
+    def print_until_end(self):
+        self.print()
+        print(self._tail, file=self._output)
 
 
 class ErrorReporter:
